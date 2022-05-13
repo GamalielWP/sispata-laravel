3fa5596f05a7edae28a3fab03d9eaa3d945748af
GamalielWP <board.dabil@gmail.com>
2022-05-13 14:44:19 +0700

    Print PDF & Error Fixing

--- resources/views/dosen/penguji.blade.php
+++ resources/views/dosen/penguji.blade.php
@@ -14,38 +14,7 @@
             </tr>
         </thead>
         <tbody>
-            {{-- <tr>
-                <td>1</td>
-                <td>18104010</td>
-                <td>Pengembangan Perangkat Lunak Dengan Metode Prototyping</td>
-                <td>Gamaliel Widhi Pradana</td>
-                <td>S1-Rekayasa Perangkat Lunak</td>
-                <td>
-                    <ul>
-                        <li><a href="">18104010-proposal.pdf </a></li>
-                        <li><a href="">18104010-berita-acara.pdf </a></li>
-                    </ul>
-                </td>
-                <td>
-                    <input type="number" class="form-control" value="90" min="0" max="100">
-                </td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>18104011</td>
-                <td>Perancangan UI/UX Aplikasi Android</td>
-                <td>Haidar 'Adiy Dzaky</td>
-                <td>S1-Rekayasa Perangkat Lunak</td>
-                <td>
-                    <ul>
-                        <li><a href="">18104010-proposal.pdf </a></li>
-                        <li><a href="">18104010-berita-acara.pdf </a></li>
-                    </ul>
-                </td>
-                <td>
-                    <input type="number" class="form-control" value="0" min="0" max="100">
-                </td>
-            </tr> --}}
+            
         </tbody>
     </table>
 @endsection
